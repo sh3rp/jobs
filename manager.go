727c0a0de52a082168a5ad2960385a8a270895a5
Shepherd Kendall <s@kndl.io>
2020-06-25 11:43:19 -0400

    using NewJob in tests

--- manager.go
+++ manager.go
@@ -4,36 +4,23 @@ type JobManager interface {
 	Submit(Job) error
 }
 
-type simpleQueueJobManager struct {
-	jobQueue chan Job
-	runner JobRunner
+type simpleJobManager struct {
+	runner   JobRunner
 }
 
-func NewSimpleQueueJobManager(queueSize int, jobRunner JobRunner) JobManager {
-	simple := simpleQueueJobManager {
-		jobQueue: make(chan Job,queueSize),
+func NewSimpleJobManager(jobRunner JobRunner) JobManager {
+	simple := simpleJobManager {
 		runner: jobRunner,
 	}
 
-	go simple.dequeingLoop()
 	return simple
 }
 
-func (sqjm simpleQueueJobManager) Submit(job Job) error {
+func (sqjm simpleJobManager) Submit(job Job) error {
 	if job.stateFunc != nil {
 		job.stateFunc(submitted("ok",nil))
 	}
-	sqjm.jobQueue <- job
+	sqjm.runner.Run(job)
 	return nil
-}
-
-func (sqjm simpleQueueJobManager) dequeingLoop() {
-	select {
-	case job :=<-sqjm.jobQueue:
-		if job.stateFunc != nil {
-			job.stateFunc(started("ok",nil))
-		}
-		sqjm.runner.Run(job)
-	}
 }
 
--- manager_test.go
+++ manager_test.go
@@ -1,14 +1,15 @@
 package jobs
 
 import (
+	"math/rand"
 	"sync"
 	"testing"
 	"time"
 )
 
-func TestJobManager(t *testing.T) {
+func TestJobManagerOneJob(t *testing.T) {
 	runner := NewBoundlessJobRunner()
-	manager := NewSimpleQueueJobManager(10,runner)
+	manager := NewSimpleJobManager(runner)
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	manager.Submit(Job{
@@ -30,5 +31,31 @@ func TestJobManager(t *testing.T) {
 			}
 		},
 	})
+	wg.Wait()
+}
+
+func TestJobManagerFiftyJobs(t *testing.T) {
+	runner := NewBoundlessJobRunner()
+	manager := NewSimpleJobManager(runner)
+	wg := sync.WaitGroup{}
+	for i := 0;i < 50;i++ {
+		wg.Add(1)
+		job := func() (map[string]interface{}, error) {
+			t.Logf("executing some bullshit")
+			time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
+			t.Logf("doing some more bullshit")
+			time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
+			return map[string]interface{}{
+				"donkey": "fucker",
+				"device": "fuck-ebay01",
+			}, nil
+		}
+		manager.Submit(NewJob(job, WithStateHandler(func(state JobState) {
+			t.Logf("[STATE] [%s] msg=%s, metadata=%+v", state.String(), state.Message(), state.Metadata())
+			if state.state == FINISHED {
+				wg.Done()
+			}
+		})))
+	}
 	wg.Wait()
 }
