c85386b6d99820d78b0616143a78e2f283abd780
Shepherd Kendall <s@kndl.io>
2020-06-26 20:36:44 -0400

    things and stuff

--- manager_test.go
+++ manager_test.go
@@ -8,8 +8,7 @@ import (
 )
 
 func TestJobManagerOneJob(t *testing.T) {
-	runner := NewBoundlessJobRunner()
-	manager := NewSimpleJobManager(runner)
+	manager := NewSimpleJobManager(NewBoundlessJobRunner())
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	manager.Submit(Job{
@@ -35,21 +34,20 @@ func TestJobManagerOneJob(t *testing.T) {
 }
 
 func TestJobManagerFiftyJobs(t *testing.T) {
-	runner := NewBoundlessJobRunner()
-	manager := NewSimpleJobManager(runner)
+	manager := NewSimpleJobManager(NewBoundlessJobRunner())
 	wg := sync.WaitGroup{}
+	job := func() (map[string]interface{}, error) {
+		t.Logf("executing some bullshit")
+		time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
+		t.Logf("doing some more bullshit")
+		time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
+		return map[string]interface{}{
+			"donkey": "fucker",
+			"device": "fuck-ebay01",
+		}, nil
+	}
 	for i := 0;i < 50;i++ {
 		wg.Add(1)
-		job := func() (map[string]interface{}, error) {
-			t.Logf("executing some bullshit")
-			time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
-			t.Logf("doing some more bullshit")
-			time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
-			return map[string]interface{}{
-				"donkey": "fucker",
-				"device": "fuck-ebay01",
-			}, nil
-		}
 		manager.Submit(NewJob(job, WithStateHandler(func(state JobState) {
 			t.Logf("[STATE] [%s] msg=%s, metadata=%+v", state.String(), state.Message(), state.Metadata())
 			if state.state == FINISHED {
