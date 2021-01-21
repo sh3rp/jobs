0768176d0320bb6b07e74edb576f4601ab3fc7e6
Shepherd Kendall <s@kndl.io>
2021-01-20 21:19:40 -0500

    added pool runner

---
+++ pooled_runner.go
@@ -0,0 +1,50 @@
+package jobs
+
+func NewPooledJobRunner(poolSize int) JobRunner {
+	return pooledJobRunner{poolSize,make(chan job),make(chan result)}
+}
+
+type pooledJobRunner struct {
+	poolSize int
+	jobsChannel chan job
+	resultChannel chan result
+}
+
+type result stuct {
+	id JobId
+	kvs []KV
+	err error
+}
+
+func (pjr pooledJobRunner) start() {
+	for i := 1 ; i <= pjr.poolSize ; i++ {
+		go worker(i,pjr.jobsChannel,pjr.resultChannel)
+	}
+}
+
+func worker(workerId int, jobs <- chan job, results <- chan result) {
+	for j := range jobs {
+		var err error
+		var metadata []KV
+		metadata = job.initialMetadata
+		defer func() {
+			errMsg := "ok"
+			if err != nil {
+				errMsg = err.Error()
+			}
+			job.stateFunc(finished(id, errMsg, metadata))
+		}()
+		job.stateFunc(started(id, "ok", metadata))
+		returnedMetadata, err := job.jobFunc()
+		if returnedMetadata != nil && len(returnedMetadata) > 0 {
+			for _, row := range returnedMetadata {
+				metadata = append(metadata, row)
+			}
+		}
+		results <- result{job.id,metadata,err}
+	}
+}
+
+func (pjr pooledJobRunner) Run(job job) {
+	pjr.jobChannel <- job
+}
--- runner.go
+++ runner.go
@@ -3,33 +3,3 @@ package jobs
 type JobRunner interface {
 	Run(job)
 }
-
-func NewBoundlessJobRunner() JobRunner {
-	return boundlessJobRunner{}
-}
-
-type boundlessJobRunner struct {
-}
-
-func (bjr boundlessJobRunner) Run(job job) {
-	id := job.id
-	go func(JobId) {
-		var err error
-		var metadata []KV
-		metadata = job.initialMetadata
-		defer func() {
-			errMsg := "ok"
-			if err != nil {
-				errMsg = err.Error()
-			}
-			job.stateFunc(finished(id, errMsg, metadata))
-		}()
-		job.stateFunc(started(id, "ok", metadata))
-		returnedMetadata, err := job.jobFunc()
-		if returnedMetadata != nil && len(returnedMetadata) > 0 {
-			for _, row := range returnedMetadata {
-				metadata = append(metadata, row)
-			}
-		}
-	}(id)
-}
